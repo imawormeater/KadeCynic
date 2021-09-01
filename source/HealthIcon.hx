ce3e9ea14d2f2a517a1f1eb4216a81106cc62d76
imawormeater <buttfacecus@gmail.com>
2021-08-31 19:32:45 -0500

    health icons

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -16,7 +16,7 @@ class HealthIcon extends FlxSprite
 		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
 
 
-		animation.add('bf', [0, 1], 0, false, isPlayer);
+		animation.add('bf', [26, 27], 0, false, isPlayer);
 		animation.add('bf-car', [0, 1], 0, false, isPlayer);
 		animation.add('bf-christmas', [0, 1], 0, false, isPlayer);
 		animation.add('bf-pixel', [21, 21], 0, false, isPlayer);
@@ -37,7 +37,7 @@ class HealthIcon extends FlxSprite
 		animation.add('parents-christmas', [17, 18], 0, false, isPlayer);
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
-		animation.add('cynic', [10, 11], 0, false, isPlayer);
+		animation.add('cynic', [24, 25], 0, false, isPlayer);
 		animation.play(char);
 		switch(char){
 			case 'bf-pixel' | 'senpai' | 'senpai-angry' | 'spirit' | 'gf-pixel':
